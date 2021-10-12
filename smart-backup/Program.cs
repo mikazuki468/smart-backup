6b4bf18d884547e864b3ae3e04ecf1bb1ea80944
nicolas.gentilucci <nicolas.gentilucci@SMARTPEG002>
2021-10-12 10:42:30 +0200

    Aggiungere i file di progetto.

---
+++ smart-backup/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace smart_backup
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
