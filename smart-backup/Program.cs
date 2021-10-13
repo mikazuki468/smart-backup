872a4981d70c52124272e939f92306714fe231e4
Nicolas Gentilucci <Nicolas.gentilucci@smartpeg.eu>
2021-10-13 16:08:27 +0200

    generazione script con parametri funzionante; output file con log di git;

--- smart-backup/Program.cs
+++ smart-backup/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using smart_backup.Service;
+using System;
+using System.Diagnostics;
+using System.IO;
 
 namespace smart_backup
 {
@@ -6,7 +9,22 @@ namespace smart_backup
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            #region Prima prova
+
+            string pathBackup = "C:\\";
+            string username = "devopsbackup";
+            string token = "4y6ttu45wxyo2gebg2xzbk3wqkte66atuhwi7xhku6zmy6g6ss3a";
+            string pathProject = "/SmartBackup2/GuruFake2/_git/GuruFake2";
+
+            Business result = new Business();
+
+            result.generateExecuteScript(username, token, pathProject, pathBackup);
+            result.generateLogGit();
+
+            #endregion
+
+
+
         }
     }
 }
---
+++ smart-backup/Service/Business.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace smart_backup.Service
+{
+    public class Business
+    {
+
+        //TODO Inserire ILogger
+
+        public void generateExecuteScript(string username, string token, string pathProject, string pathBackup)
+        {
+            string resultScript = string.Format("clone https://{0}:{1}@dev.azure.com{2}",username,token,pathProject);
+            //ProcessStartInfo p = new ProcessStartInfo();
+
+            #region Prima prova 
+            //p.FileName = "git.exe";
+            //p.Arguments = resultScript;
+            //p.UseShellExecute = false;
+            //p.RedirectStandardOutput = true;
+
+            //Process proStart = new Process();
+            //proStart.StartInfo = p;
+            //proStart.Start();            
+            //await File.WriteAllTextAsync("output.txt",proStart.StandardOutput.ReadToEnd());
+            //proStart.WaitForExit();
+            ////return resultScript;
+            #endregion
+
+            using (Process process = new Process())
+            {
+
+                process.StartInfo.WorkingDirectory = pathBackup;
+                process.StartInfo.FileName = "git.exe";
+                process.StartInfo.UseShellExecute = false;
+                process.StartInfo.RedirectStandardOutput = true;
+                process.StartInfo.Arguments = resultScript;
+                process.Start();
+
+                // Synchronously read the standard output of the spawned process.
+                StreamReader reader = process.StandardOutput;
+                string output = reader.ReadToEnd();
+
+                // Write the redirected output to this application's window.
+                Console.WriteLine(output);
+                //File.WriteAllText("output.txt", output);
+
+                process.WaitForExit();
+
+            }
+
+        }
+
+        public void generateLogGit()
+        {
+            string log = string.Format("log");
+            
+            using (Process process = new Process())
+            {
+                process.StartInfo.FileName = "git.exe";
+                process.StartInfo.UseShellExecute = false;
+                process.StartInfo.RedirectStandardOutput = true;
+                process.StartInfo.Arguments = log;
+                process.Start();
+
+                StreamReader reader = process.StandardOutput;
+                string output = reader.ReadToEnd();
+
+                File.WriteAllText("output.txt", output);
+
+                process.WaitForExit();
+            }
+
+            Console.WriteLine("\n\nPress any key to exit.");
+            Console.ReadLine();
+        }
+
+    }
+
+    
+}
