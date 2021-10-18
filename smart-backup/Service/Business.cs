3787bd7dd0b40c686fda7c181d4407afe8e21619
Nicolas.Gentilucci <nicolas.gentilucci@smartpeg.eu>
2021-10-18 12:54:21 +0200

    Merge Serilog in develops

--- smart-backup/Service/Business.cs
+++ smart-backup/Service/Business.cs
@@ -1,5 +1,8 @@
 ﻿using LibGit2Sharp;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using Serilog;
+using Serilog.Events;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
@@ -11,43 +14,62 @@ namespace SmartBackup.service
 {
     public class Business
     {
-
-        private readonly ILogger _logger;
-        public Business(ILogger<Business> logger)
-        {
-            _logger = logger;
-
-        }
         internal void Run()
         {
-            _logger.LogInformation("Application Started at {dateTime}", DateTime.UtcNow);
+            Log.Logger = new LoggerConfiguration()
+            //.ReadFrom.AppSettings()
+            .MinimumLevel.Debug()
+            .WriteTo.File("output.txt", restrictedToMinimumLevel: LogEventLevel.Information, rollingInterval: RollingInterval.Day)
+            .WriteTo.Console(restrictedToMinimumLevel: LogEventLevel.Information)
+            .CreateLogger();
+
+            Log.Information("Application Started at {dateTime}", DateTime.UtcNow);
 
             //Business Logic START
 
-            generateExecuteScript();
+            try
+            {
+                generateExecuteScript();
+            }
+            catch (Exception ex)
+            {
+
+                Log.Error(ex, "Qualcosa è andato storto");
+            }
+
 
             //Business logic END
-            _logger.LogInformation("Application Ended at {dateTime}", DateTime.UtcNow);
+            Log.Information("Application Ended at {dateTime}", DateTime.UtcNow);
         }
 
         private void generateExecuteScript()
         {
             using (Process process = new Process())
             {
-                
+
                 string gitUser = "devopsbackup";
                 string gitToken = "4y6ttu45wxyo2gebg2xzbk3wqkte66atuhwi7xhku6zmy6g6ss3a";
                 string sourceUrl = "https://SmartBackup2@dev.azure.com/SmartBackup2/GuruFake2/_git/GuruFake2";
 
                 string nameProject = "GuruFake2";
-                string workdirPath = "C:\\"+nameProject;
+                string workdirPath = "C:\\" + nameProject;
                 CloneOptions co = new CloneOptions();
                 co.CredentialsProvider = (_url, _user, _cred) => new UsernamePasswordCredentials { Username = gitUser, Password = gitToken };
-                _logger.LogInformation("inizio clonazione repo {0}", sourceUrl);
-                
-                
-                LibGit2Sharp.Repository.Clone(sourceUrl, workdirPath, co);
-                
+
+                Log.Information("inizio clonazione repo {0}", sourceUrl);
+
+                try
+                {
+                    LibGit2Sharp.Repository.Clone(sourceUrl, workdirPath, co);
+
+                }
+                catch (Exception ex)
+                {
+                    Log.Warning("Attenzione {0} già esistente", nameProject);
+                    Log.Information(ex, "Verificare errore seguente:");
+                }
+
+
             }
         }
 
