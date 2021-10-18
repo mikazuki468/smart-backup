550e3eaed8641866771508aff47fe1c7c71ae1f5
Roberto Galanti <roberto.galanti@smartpeg.eu>
2021-10-18 15:18:05 +0200

    model

---
+++ smart-backup/DTO/GitOrgDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace smart_backup.Model
+{
+    class GitOrgDTO
+    {
+        public string NameOrganization { get; set; }
+        public List<GitRepoDTO> Project { get; set;}
+    }
+}
---
+++ smart-backup/DTO/GitRepoDTO.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace smart_backup.Model
+{
+    public class GitRepoDTO
+    {
+        public string id { get; set; }
+        public string name { get; set; }
+        public string url { get; set; }
+        public string state { get; set; }
+        public int revision { get; set; }
+        public string visibility { get; set; }
+        public DateTime lastUpdateTime { get; set; }
+
+    }
+}
